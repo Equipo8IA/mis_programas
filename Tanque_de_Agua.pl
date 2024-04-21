29f2f831ba8443028f33553688bd502d82e08eaf
Equipo8IA <159973952+Equipo8IA@users.noreply.github.com>
2024-04-21 19:13:45 -0400

    Actualizar Tanque_de_Agua.pl
    cambios temporales

--- Tanque_de_Agua.pl
+++ Tanque_de_Agua.pl
@@ -1,19 +1,14 @@
+ 
 % Hechos
-
-%X
 tanque_poca_agua(encendido).
-
-%Y
 cisterna_poca_agua(encendido).
-
-%Z
 turbina(apagado).
-
-%T
 taque_lleno(encendido).
 
- % Regla_1
-encender_turbina(X, Y, Z):- tanque_poca_agua(X), cisterna_poca_agua(Y), turbina(Z), Z == apagado ; tanque_poca_agua(X), cisterna_poca_agua(Y), Y == apagado.
+% Regla_1
+encender_turbina(X, Y, Z):- tanque_poca_agua(X), cisterna_poca_agua(Y), turbina(Z), Z == apagado.
+encender_turbina(X, Y, Z):- tanque_poca_agua(X), cisterna_poca_agua(Y), Z == apagado.
 
 % Regla_2
-apagar_turbina(X, Y, T):- turbina(X), taque_lleno(T), X == encendido ; turbina(X), cisterna_poca_agua(Y), X == encendido.
+apagar_turbina(X, Y, T):- turbina(X), taque_lleno(T), X == encendido.
+apagar_turbina(X, Y, T):- turbina(X), cisterna_poca_agua(Y), X == encendido.

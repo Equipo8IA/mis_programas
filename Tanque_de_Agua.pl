81f759fa587617adf4b3296b2878c1e6e56ce6b0
Equipo8IA <159973952+Equipo8IA@users.noreply.github.com>
2024-04-22 21:54:58 -0400

    Tanque_de_Agua.pl
    versión final

--- Tanque_de_Agua.pl
+++ Tanque_de_Agua.pl
@@ -1,14 +1,22 @@
- 
-% Hechos
-tanque_poca_agua(encendido).
-cisterna_poca_agua(encendido).
-turbina(apagado).
-taque_lleno(encendido).
+% Hechos:
+tanque_poca_agua(apagado).
+cisterna_poca_agua(apagado).
+turbina(encendido).
+tanque_lleno(apagado).
 
-% Regla_1
-encender_turbina(X, Y, Z):- tanque_poca_agua(X), cisterna_poca_agua(Y), turbina(Z), Z == apagado.
-encender_turbina(X, Y, Z):- tanque_poca_agua(X), cisterna_poca_agua(Y), Z == apagado.
+% Regla:
+encender_turbina( X, Y, T, Z):-tanque_poca_agua(X), cisterna_poca_agua(Y), turbina(T), tanque_lleno(Z); V=encendido, X = V, cisterna_poca_agua(Y), turbina(T), tanque_lleno(Z); tanque_poca_agua(X), cisterna_poca_agua(Y), V=apagado, T = V, tanque_lleno(Z); V=encendido, X = V, cisterna_poca_agua(Y), A=apagado, T = A, tanque_lleno(Z).
 
-% Regla_2
-apagar_turbina(X, Y, T):- turbina(X), taque_lleno(T), X == encendido.
-apagar_turbina(X, Y, T):- turbina(X), cisterna_poca_agua(Y), X == encendido.
+
+%Readme:
+% X para tanque_poca_agua
+% Y para cisterna_poca_agua
+% T para turbina
+% Z para tanque_lleno
+
+% Las letras V y A solo son usadas como varibles para comparar con el resultado deseado
+
+% Ojo la interpretacionde los hechos es de manera que su nombre se refiere a la condicion del sensor 
+% ejemplo: Si hay "poca agua en el tanque" el sencosr se = enciende 
+% en el caso de turbina esta fefinida como encendida, pero en si el programa Python es el que activa la verdadera Turbina 
+% no olvidar que esto es más una Desmotración(simulación)
